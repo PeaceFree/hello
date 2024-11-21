5cbf9b06b3f49ae0112f7683bf8c1ab2b3221d05
PeaceFree <gurkha@live.com>
2024-11-20 19:38:10 -0800

    update background

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { useState } from 'react';
 function App() {
   const [data, setData] = useState('')
   const [test, set] = useState('Click Button Test')
+  // const [AppState, setApp] = useState('background: "red"')
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" >
+      <header className="App-header" id='style' > 
          <p>
            {test}
+         
         </p>
-        <button  className="button-30" onClick={()=>{setData('Hello World'); set('') }}>UPDATE</button>
+        <button className="button-30" onClick={()=>{setData('Hello World');
+                document.getElementById("style").style.background ='#003c00 ';
+                 set('') }}>UPDATE</button>
 
         <p>{data}</p>
        
